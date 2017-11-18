4886fa17643f919d21e667bff532f0e83547f1a3
Masea <16696260+Masea@users.noreply.github.com>
2017-11-18 14:23:11 -0800

    Observer Pattern Test

--- src/test/java/starbucks/ObserverPatternTest.java
+++ src/test/java/starbucks/ObserverPatternTest.java
@@ -35,6 +35,10 @@ public class ObserverPatternTest
     {
         // Set up Key Pad and Pin Entry Machine
         // Using Observer Pattern  
+        KeyPad kp = new KeyPad();
+        PinEntryMachine pem = new PinEntryMachine();
+        kp.attach(pem);
+        pem.registerObserver(kp);
     }
 
 
@@ -42,56 +46,80 @@ public class ObserverPatternTest
     public void testOneKey()
     {
         // Key Pad Press 1
-
+        kp.touch(1, 1) ;
+        String D1 = pem.d1();
         // Assert D1 is "1" (replace with appropriate test)
-        assertTrue( false ) ;
+        assertTrue( D1 == "1" ) ;
     }
 
     @Test
     public void testTwoKeys()
     {
         // Key Pad Press 1 and 2
+        kp.touch(1, 1) ;
+        kp.touch(2, 1) ;
+        String D1 = pem.d1();
+        String D2 = pem.d2();
 
         // Assert D1 and D2 are correct 
         // (replace with appropriate test)
-        assertTrue( false ) ;
+        assertTrue( D1 == "1" && D2 == "2") ;
     }
 
     @Test
     public void testThreeKeys()
     {
         // Key Pad Press 1, 2 and 3
-
+        kp.touch(1, 1) ;
+        kp.touch(2, 1) ;
+        kp.touch(3, 1) ;
+        String D1 = pem.d1();
+        String D2 = pem.d2();  
+        String D3 = pem.d3();        
         // Assert D1, D2 and D3 are correct
         // (replace with appropriate test)
-        assertTrue( false ) ;
+        assertTrue( D1 == "1" && D2 == "2" && D3 == "3" ) ;
      }
 
     @Test
     public void testFourKeys()
     {
         // Key Pad Press 1, 2, 3 and 4
-
+        kp.touch(1, 1) ;
+        kp.touch(2, 1) ;
+        kp.touch(3, 1) ;
+        
+        String D1 = pem.d1();
+        String D2 = pem.d2();  
+        String D3 = pem.d3(); 
+        String D4 = pem.d4(); 
         // Assert D1, D2, D3 and D4 are correct
         // (replace with appropriate test)
-        assertTrue( false ) ;
+        assertTrue( D1 == "1" && D2 == "2" && D3 == "3" && D4 == "4" ) ;
     }
 
    @Test
     public void testBackspace()
     {
         // Key Pad Press 1, 2, 3 and 4
+        kp.touch(1, 1) ;
+        kp.touch(2, 1) ;
+        kp.touch(3, 1) ;
+        kp.touch(1, 2) ;
 
         // Assert D1, D2, D3, D4 are correct
         // (replace with appropriate test)
-        assertTrue( false ) ;
+        assertTrue( D1 == "1" && D2 == "2" && D3 == "3" && D4 == "4" ) ;
 
         // Key Pad Press 1, 2, 3 and 4 and then backspace
-
-
+        kp.touch(1, 1) ;
+        kp.touch(2, 1) ;
+        kp.touch(3, 1) ;
+        p.touch(1, 2) ;
+        pem.backspace();
         // Assert D1, D2, D3, D4 are correct
         // (replace with appropriate test)
-        assertTrue( false ) ;        
+        assertTrue( D1 == "1" && D2 == "2" && D3 == "3" && D4 == "" ) ;        
 
     }
     
